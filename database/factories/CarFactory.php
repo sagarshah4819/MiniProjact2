4d59359453de9b675388dc78f51c467d4335b14d
sagarshah4819 <ss3958@njit.edu>
2018-10-28 13:45:49 -0700

    Task: Create a database seed to generate 50 cars.

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\car::class, function (Faker $faker) {
     return [
 
         'Make' => $faker->randomElement($array = array ('Ford','Honda','Toyota')),
-        'Model' => $faker->colorName,
+        'Model' => $faker->randomElement($array = array ('Hatchback','Sedan','SUV')),
         'Year'=> $faker->year($max='now'),
 
     ];
---
+++ database/seeds/CarsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CarsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+        factory(App\car::class, 50)->create()->each(function ($u) {
+            // $u->posts()->save(factory(App\car::class)->make());
+        });
+    }
+}
