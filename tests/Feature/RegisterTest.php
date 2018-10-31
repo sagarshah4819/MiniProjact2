a414337e71f0c68e867292463d713c39bc21e6b3
sagarshah4819 <ss3958@njit.edu>
2018-10-31 10:37:53 -0700

    Task(Test 4): Feature Test 4 to check contact page returns status code 200

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -30,4 +30,10 @@ class RegisterTest extends TestCase
         $response = $this->get('/about');
         $response->assertStatus(200);
     }
+
+    public function testContactPage()
+    {
+        $response = $this->get('/contact');
+        $response->assertStatus(200);
+    }
 }
